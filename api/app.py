af97c23fe13e6d5fc281c46830565cc30358b56e
gwvsol <gwvsol@gmail.com>
2019-02-01 16:38:37 +0200

    added method verify password

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_httpauth import HTTPBasicAuth
+from hashlib import md5
 from config import Config
 
 app = Flask(__name__)
@@ -8,21 +9,30 @@ auth = HTTPBasicAuth()
 app.config.from_object(Config)
 api = Api(app)
 
-@auth.get_password
-def get_pw(username):
-    users = app.config['USERS']
-    if username in users:
-        return users.get(username)
-    return None
+def setpasswd(login:str, passw:str) -> str:
+    """Преобразование пароля и логина в хеш MD5"""
+    return str(md5(str(passw+login).lower().encode('utf-8')).hexdigest())
     
+@auth.verify_password
+def verify_password(username, password):
+    """Проверка пароля и логина"""
+    users = app.config['USERS']
+    if username.lower() in users.keys():
+        if users[str(username.lower())] == setpasswd(username.lower(), password):
+            return users[str(username.lower())] #При совпедении пароля, возвращаем хеш пароля из БД
+    return None
+
 class Main(Resource):
     @auth.login_required
     def get(self):
-        return {"GET message": app.config['USERS']}
+        return {'GET message': 'GET !!!!!!'}
     
     @auth.login_required
     def post(self):
-        return {"POST message": "Hello, World!"}
+        return {'POST message': 'POST !!!!!!!'}
 
 # Route
 api.add_resource(Main, '/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
--- api/config.py
+++ api/config.py
@@ -1,5 +1,4 @@
 class Config(object):
     DB_CONFIG = {'host': 'localhost', 'port': '28015', 'db': 'db_app', }
     DB_TAB = {'t1': 'data', 't2': 'users', 't3': 'logs', }
-    USERS = { "john": "hello", "susan": "bye" }
-    
+    USERS = { 'john': '018ff8ac396bc810a93e2f84dfbb1714', 'susan': '35699bdb666b55656fabd4a201c6291f' }
