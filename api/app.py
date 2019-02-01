db4ea62beb5c69d09dd71b29c0c8653e2aadfce9
gwvsol <gwvsol@gmail.com>
2019-02-01 18:51:16 +0200

    chenge methods everyone and oneof

--- api/app.py
+++ api/app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, abort, jsonify, make_response
 from flask_httpauth import HTTPBasicAuth
 from hashlib import md5
 from config import Config
@@ -7,32 +6,57 @@ from config import Config
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 app.config.from_object(Config)
-api = Api(app)
+
 
 def setpasswd(login:str, passw:str) -> str:
     """Преобразование пароля и логина в хеш MD5"""
     return str(md5(str(passw+login).lower().encode('utf-8')).hexdigest())
-    
+
+
 @auth.verify_password
 def verify_password(username, password):
     """Проверка пароля и логина"""
-    users = app.config['USERS']
+    users = app.config['DATA']
+    #Если login найден в БД, проверяем совпадает ли passwd
     if username.lower() in users.keys():
-        if users[str(username.lower())] == setpasswd(username.lower(), password):
-            return users[str(username.lower())] #При совпедении пароля, возвращаем хеш пароля из БД
+        if users[str(username.lower())]['passw'] == setpasswd(username.lower(), password):
+    #При совпедении passwd, возвращаем хеш passwd из БД
+            return users[str(username.lower())]
     return None
 
-class Main(Resource):
-    @auth.login_required
-    def get(self):
-        return {'GET message': 'GET !!!!!!'}
-    
-    @auth.login_required
-    def post(self):
-        return {'POST message': 'POST !!!!!!!'}
 
-# Route
-api.add_resource(Main, '/')
+@app.errorhandler(400)
+def bad_request(error):
+    """Ответ при неправильном запросе"""
+    return make_response(jsonify({'error': 'BadRequest'}), 400)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Ответ при отсутствии данных в базе"""
+    return make_response(jsonify({'error': 'NotFound'}), 404)
+
+
+@app.route('/api', methods=['GET','POST'])
+@auth.login_required
+def everyone():
+    """Запрос данных о всех пользователях"""
+    # curl -s -u john:hello -X POST http://192.168.0.12:5000/api
+    # curl -s -u john:hello -G http://192.168.0.12:5000/api
+    return jsonify(app.config['DATA'])
+
+
+@app.route('/api/<todo>', methods=['GET','POST'])
+@auth.login_required
+def oneof(todo):
+    """Запрос данных по конкретному пользователю"""
+    if todo in app.config['DATA'].keys():
+    # curl -s -u john:hello -X POST http://192.168.0.12:5000/api/susan
+    # curl -s -u john:hello -G http://192.168.0.12:5000/api
+        return jsonify({str(todo): app.config['DATA'][str(todo)]})
+    else:
+        abort(404)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
--- api/config.py
+++ api/config.py
@@ -1,4 +1,5 @@
 class Config(object):
     DB_CONFIG = {'host': 'localhost', 'port': '28015', 'db': 'db_app', }
     DB_TAB = {'t1': 'data', 't2': 'users', 't3': 'logs', }
-    USERS = { 'john': '018ff8ac396bc810a93e2f84dfbb1714', 'susan': '35699bdb666b55656fabd4a201c6291f' }
+    DATA = { 'john': {'name': None, 'passw': '018ff8ac396bc810a93e2f84dfbb1714', 'gender': None, 'reg_date': None, 'phone': None, 'email': None}, \
+            'susan': {'name': None, 'passw': '35699bdb666b55656fabd4a201c6291f', 'gender': None, 'reg_date': None, 'phone': None, 'email': None} }
