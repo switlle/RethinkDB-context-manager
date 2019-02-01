88bb60412175e4dbb0f5f40a9ce6329f21775ce9
gwvsol <gwvsol@gmail.com>
2019-02-01 14:40:19 +0200

    debug method resource

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_restful import Api
+
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+api = Api(app)
 
-from app import routers
+from webapp import resources
+from webapp.resources import Main
+
+# Route
+api.add_resource(Main, '/')
--- webapp/resources.py
+++ webapp/resources.py
@@ -1,5 +1,9 @@
-from app import app
+from webapp import app
+from flask_restful import Resource
 
-@app.route('/')
-def api():
-    return "Hellow World"
+class Main(Resource):
+    def get(self):
+        return {"GET message": "Hello, World!"}
+        
+    def post(self):
+        return {"POST message": "Hello, World!"}
