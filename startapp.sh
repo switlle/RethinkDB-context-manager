c1646e3b1625f951b0817850a40fb1b9940d6bfc
gwvsol <gwvsol@gmail.com>
2019-02-03 22:58:44 +0200

    the conversion of the project

--- startapp.sh
+++
@@ -1,8 +0,0 @@
-DIR="/app/wsgi"
-cd $DIR
-source venv/bin/activate
-#export FLASK_APP=restapi.py
-#export FLASK_DEBUG=1
-#export FLASK_RUN_HOST=0.0.0.0
-#export FLASK_RUN_PORT=5000
-screen python3.6 api/app.py
