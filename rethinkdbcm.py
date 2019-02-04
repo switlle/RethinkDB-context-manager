6d6b140efe5d8f154c9b08f4c729d88d1565d6b6
gwvsol <gwvsol@gmail.com>
2019-02-04 10:30:42 +0200

    changes for pep8 compliance

--- rethinkdbcm.py
+++ rethinkdbcm.py
@@ -1,6 +1,7 @@
 import rethinkdb as db
 from rethinkdb.errors import ReqlOpFailedError
 
+
 class UseDatabase:
     def __init__(self, config: dict, db=None) -> None:
         self.config = config
@@ -16,52 +17,52 @@ class UseDatabase:
         try:
             return db.db_list().run()
         except ReqlOpFailedError:
-            return { self.config['db']: 'does not exist' }
-        
+            return {self.config['db']: 'does not exist'}
+
     def create_db(self, name):
         """Создание новой базы данных"""
-        try: 
+        try:
             return db.db_create(name).run()
         except ReqlOpFailedError:
-            return { name: 'the database is already there' }
-        
+            return {name: 'the database is already there'}
+
     def del_db(self, name):
         """Удаление базы данных"""
         try:
             return db.db_drop(name).run()
         except ReqlOpFailedError:
-            return { name: 'database not found' }
+            return {name: 'database not found'}
 
     def all_table(self, name_db):
         """Получение всех таблиц из подключенной БД"""
         try:
             t = db.table_list().run(self.conn)
-            if not t: #Если таблиц нет
-                t = { 'table in DB ' + name_db: 'not found' }
+            if not t:  # Если таблиц нет
+                t = {'table in DB ' + name_db: 'not found'}
             return t
         except ReqlOpFailedError:
-            return { 'DB ' + name_db: 'does not exist' }
+            return {'DB ' + name_db: 'does not exist'}
 
     def create_tab(self, name_db, name_t):
         """Создание новой таблицы в базе данных"""
-        try: 
+        try:
             return db.db(name_db).table_create(name_t).run()
         except ReqlOpFailedError:
-            return { name_t + ' in DB ' + name_db: 'the table already exists' }
+            return {name_t + ' in DB ' + name_db: 'the table already exists'}
 
     def del_tab(self, name_db, name_t):
         """Удаление таблицы из базы данных"""
         try:
             return db.db(name_db).table_drop(name_t).run()
         except ReqlOpFailedError:
-            return { name_t + ' in DB ' + name_db: 'the table does not exist' }
+            return {name_t + ' in DB ' + name_db: 'the table does not exist'}
 
     def insert(self, name_t, json):
         """Добавление новой записи в таблицу БД"""
         try:
             return db.table(name_t).insert(json).run()
         except ReqlOpFailedError:
-            return { name_t: 'the table does not exist' }
+            return {name_t: 'the table does not exist'}
 
     def countid(self, name_t, id_name, req):
         """Определение записи в таблице БД по определнному ID
@@ -77,7 +78,7 @@ class UseDatabase:
 
     def getroot(self, name_t):
         """Получение записи только из первой таблицы (root) базы данных"""
-        #Метод является частным случаем
+        # Метод является частным случаем
         try:
             return list(db.table(name_t).run())[0]
         except (ReqlOpFailedError, IndexError):
