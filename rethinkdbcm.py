620df8eb87f9406577b1cc9e9619f64e47814fd8
gwvsol <gwvsol@gmail.com>
2019-02-10 23:37:01 +0200

    updated methods of working with the database

--- rethinkdbcm.py
+++ rethinkdbcm.py
@@ -1,11 +1,11 @@
+# -*- coding: utf-8 -*-
 import rethinkdb as db
 from rethinkdb.errors import ReqlOpFailedError
 
 
 class UseDatabase:
-    def __init__(self, config: dict, db=None) -> None:
+    def __init__(self, config: dict) -> None:
         self.config = config
-        self.config['db'] = db
 
     def __enter__(self):
         """Подключение к базе данных RethinkDB"""
@@ -19,19 +19,19 @@ class UseDatabase:
         except ReqlOpFailedError:
             return {self.config['db']: 'does not exist'}
 
-    def create_db(self, name):
+    def create_db(self, name_db):
         """Создание новой базы данных"""
         try:
-            return db.db_create(name).run()
+            return db.db_create(name_db).run()
         except ReqlOpFailedError:
-            return {name: 'the database is already there'}
+            return {name_db: 'the database is already there'}
 
-    def del_db(self, name):
+    def del_db(self, name_db):
         """Удаление базы данных"""
         try:
-            return db.db_drop(name).run()
+            return db.db_drop(name_db).run()
         except ReqlOpFailedError:
-            return {name: 'database not found'}
+            return {name_db: 'database not found'}
 
     def all_table(self, name_db):
         """Получение всех таблиц из подключенной БД"""
@@ -57,56 +57,58 @@ class UseDatabase:
         except ReqlOpFailedError:
             return {name_t + ' in DB ' + name_db: 'the table does not exist'}
 
-    def insert(self, name_t, json):
+    def insert(self, name_db, name_t, json):
         """Добавление новой записи в таблицу БД"""
         try:
-            return db.table(name_t).insert(json).run()
+            return db.db(name_db).table(name_t).insert(json).run()
         except ReqlOpFailedError:
             return {name_t: 'the table does not exist'}
 
-    def countid(self, name_t, id_name, req):
+    def countid(self, name_db, name_t, id_name, req):
         """Определение записи в таблице БД по определнному ID
         0 - записи нет, 1 - запись имеется"""
-        return db.table(name_t).filter({id_name: req}).count().run()
+        return db.db(name_db).table(name_t).filter({id_name: req}).count().run()
 
-    def gettasks(self, name_t):
+    def gettasks(self, name_db, name_t):
         """Получение всех записей из таблицы базы данных"""
         try:
-            return list(db.table(name_t).run())
+            return list(db.db(name_db).table(name_t).run())
         except (ReqlOpFailedError, IndexError):
             return None
 
-    def getroot(self, name_t):
+    def getroot(self, name_db, name_t):
         """Получение записи только из первой таблицы (root) базы данных"""
         # Метод является частным случаем
         try:
-            return list(db.table(name_t).run())[0]
+            return list(db.db(name_db).table(name_t).run())[0]
         except (ReqlOpFailedError, IndexError):
             return None
 
-    def gettask(self, name_t, req):
+    def gettask(self, name_db, name_t, req):
         """Получение записей из таблицы базы данных по определенному ID"""
         try:
-            return db.table(name_t).get(req).run()
+            return db.db(name_db).table(name_t).get(req).run()
         except ReqlOpFailedError:
             return None
 
-    def updetask(self, name_t, id_name, json):
+    def updetask(self, name_db, name_t, id_name, json):
         """Обновление записи в таблице БД по определнному ID"""
-        return db.table(name_t).get(id_name).update(json).run()
+        return db.db(name_db).table(name_t).get(id_name).update(json).run()
 
-    def delltask(self, name_t, id_name):
+    def delltask(self, name_db, name_t, id_name):
         """Удаление записи в таблице БД по определнному ID"""
-        return db.table(name_t).get(id_name).delete().run()
+        return db.db(name_db).table(name_t).get(id_name).delete().run()
 
-    def getval(self, name_t, id_name):
+    def getval(self, name_db, name_t, id_name):
         """Получение значений, без ключей из таблицы БД по определенному ID"""
-        return list(db.table(name_t).get(id_name).values().run())
+        return list(db.db(name_db).table(name_t).get(id_name).values().run())
 
-    def countall(self, name_t):
+    def countall(self, name_db, name_t):
         """Определение количества записи в таблице БД"""
-        return db.table(name_t).count().run()
+        return db.db(name_db).table(name_t).count().run()
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         """Отключение от БД"""
         self.conn.close()
+
+
